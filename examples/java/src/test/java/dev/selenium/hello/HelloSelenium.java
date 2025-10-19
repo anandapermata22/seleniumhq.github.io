c7a1b98a5ffce169bf019d9c66413739bfc80997
anandapermata22 <anandapermatawork@gmail.com>
2025-10-19 18:09:23 +0700

    Update HelloSelenium.java

--- examples/java/src/test/java/dev/selenium/hello/HelloSelenium.java
+++ examples/java/src/test/java/dev/selenium/hello/HelloSelenium.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class HelloSelenium {
     public static void main(String[] args) {
         WebDriver driver = new ChromeDriver();
-
+https://github.com/bestiejs/punycode.js/blob/master/punycode.js
         driver.get("https://selenium.dev");
 
         driver.quit();
